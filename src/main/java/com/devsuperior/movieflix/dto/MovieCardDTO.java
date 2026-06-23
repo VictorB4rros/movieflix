9a64719d9e3b5370dc13102c734fc1092f0b6377
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2026-06-22 22:03:13 -0300

    Movie repository, service and controller

--- src/main/java/com/devsuperior/movieflix/dto/MovieCardDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/MovieCardDTO.java
@@ -1,5 +1,7 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.projections.MovieProjection;
+
 public class MovieCardDTO {
 
     private Long id;
@@ -7,6 +9,17 @@ public class MovieCardDTO {
     private String subTitle;
     private Integer year;
     private String imgUrl;
+
+    public MovieCardDTO() {
+    }
+
+    public MovieCardDTO(MovieProjection projection) {
+        this.id = projection.getId();
+        this.title = projection.getTitle();
+        this.subTitle = projection.getSubTitle();
+        this.year = projection.getYear();
+        this.imgUrl = projection.getImgUrl();
+    }
 
     public Long getId() {
         return id;
---
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.projections.MovieProjection;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<MovieCardDTO> findByGenre(Long genreId, Pageable pageable) {
+        Page<MovieProjection> result;
+        if (genreId == 0) {
+            result = repository.searchAllMovies(pageable);
+        } else {
+            result = repository.searchAllMoviesByGenre(genreId, pageable);
+        }
+        return result.map(MovieCardDTO::new);
+    }
+}
